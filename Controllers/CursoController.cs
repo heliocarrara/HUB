b98eadb1ff2820e218125c58fdc9360026cb20a2
heliocarrara <heliocarrara@gmail.com>
2023-06-02 14:54:04 -0400

    Criação de página para cursos disponíveis.

---
+++ Controllers/CursoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HUB.Controllers
+{
+    public class CursoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult CursosDisponiveis()
+        {
+            try
+            {
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return Index();
+            }
+        }
+    }
+}
